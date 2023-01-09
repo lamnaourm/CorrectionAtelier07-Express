29587744183701a9c268cd1d6ba62d6c5c259fed
lamnaourm <mohammed.lamnaour@gmail.com>
2023-01-09 15:16:31 +0100

    schema de user

--- models/UserSchema.js
+++ models/UserSchema.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const {Schema} = require('mongoose');
 
 const User = new Schema({
-
+    login: {type: String, require: true, unique:true},
+    firstname: String,
+    lastname: String,
+    mail: {type: String, require: true, unique:true},
+    password:String
 }, {collection: 'users'})
 
 module.exports = mongoose.model("users", User, "users");
